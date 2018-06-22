8e539caee447ae79b338acf245ca35bdd27675a6
Eaman Jahani <eamanj@gmail.com>
2018-06-22 11:47:11 -0400

    first commit

---
+++ clean_data.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import argparse
+from collections import defaultdict
+import pandas
+import sys
+
+
+def main():
+  print("hello first version")
+
+
+if __name__ == "__main__":
+  main()
